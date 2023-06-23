608c91fd1f93c79ef7cc8aecec90ce63819c5d82
Rachel Lee <rachellee040@gmail.com>
2023-06-23 17:56:58 -0400

    dark mode & initial more page

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-const themeToggle = document.getElementById("theme-toggle");
-const body = document.body;
+const themeToggle = document.querySelector('#theme-toggle');
+const root = document.documentElement;
+
+const setTheme = (theme) => {
+    if (theme === 'dark') {
+        themeToggle.checked = true;
+        root.setAttribute('data-theme', 'dark');
+    } else {
+        themeToggle.checked = false;
+        root.removeAttribute('data-theme');
+    }
+    
+};
+
+const toggleTheme = () => {
+    if (themeToggle.checked) {
+        localStorage.setItem('theme', 'dark');
+        root.setAttribute('data-theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+        root.setAttribute('data-theme', 'light');
+    }
+};
+
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 });
 
-themeToggle.addEventListener("change", function() {
-    body.classList.toggle("dark-mode");
-    localStorage.setItem("darkMode", themeToggle.checked);
-});
+themeToggle.addEventListener('change', toggleTheme);
 
-const darkMode = localStorage.getItem("darkMode");
-themeToggle.checked = darkMode === "true";
-
-if (themeToggle.checked) {
-    body.classList.add("dark-mode");
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+    setTheme(storedTheme);
+} else {
+    const prefersDarkMode = window.matchMedia('(perfers-color-scheme: dark)').matches;
+    setTheme(prefersDarkMode ? 'dark' : 'light');
 }
 
