a12cd67b718aa2bbd52c2e53cf40ce7676de4b11
Rachel Lee <rachellee040@gmail.com>
2023-06-22 23:09:21 -0400

    dark mode

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 });
+
+themeToggle.addEventListener("change", function() {
+    body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode", themeToggle.checked);
+});
+
+const darkMode = localStorage.getItem("darkMode");
+themeToggle.checked = darkMode === "true";
+
+if (themeToggle.checked) {
+    body.classList.add("dark-mode");
+}
 
